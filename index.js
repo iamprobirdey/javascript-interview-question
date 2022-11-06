dbe30439cd3bcf2eec3a2e21167ae3491ecd58ae
Probir Dey <probir@Probirs-MacBook-Air.local>
2022-11-06 11:42:45 +0530

    index.js changes

---
+++ index.js
@@ -0,0 +1,22 @@
+// what are the possible ways to create a object.
+
+// 1. Object Contructor:
+    var object = new Object();
+
+// 2. Object create method:
+    var object = Object.create(null);
+
+// 3. Object Literal syntax:
+    var object = {
+        firstName: 'Probir',
+        lastName: 'Dey'
+    }
+
+// 4. Function Contructor
+    function Person(name){
+        this.name = name,
+        this.age = '31'
+    }
+
+// 5. 
+
